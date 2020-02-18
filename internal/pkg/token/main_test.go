a7b99f52b5c8400c366b96df057ef88b29b5c337
Adam Hughes <stickmanica@gmail.com>
2020-02-18 17:34:20 -0500

    Implement token tests

---
+++ internal/pkg/token/main_test.go
@@ -0,0 +1,73 @@
+// Copyright (c) 2020, Sylabs, Inc. All rights reserved.
+
+package token
+
+import (
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var (
+	testSigningMethod = jwt.SigningMethodHS256
+	testSigningKey    = []byte("AllYourBase")
+
+	testClaims               *Claims
+	testToken                string
+	testTokenNoClaims        string
+	testTokenInvalidAudience string
+	testTokenInvalidIssuer   string
+	testTokenExpired         string
+)
+
+func makeToken(c *Claims) string {
+	s, err := jwt.NewWithClaims(testSigningMethod, c).SignedString(testSigningKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return s
+}
+
+func TestMain(m *testing.M) {
+	// Claims for testing.
+	testClaims = &Claims{
+		jwt.StandardClaims{
+			Id:        "id",
+			Subject:   "subject",
+			Audience:  "api://default",
+			Issuer:    "https://example.com",
+			IssuedAt:  time.Now().UTC().Unix(),
+			ExpiresAt: time.Now().Add(time.Hour).UTC().Unix(),
+		},
+	}
+	testToken = makeToken(testClaims)
+
+	// No claims.
+	testTokenNoClaims = makeToken(&Claims{})
+
+	// Bad audience value.
+	testTokenInvalidAudience = makeToken(&Claims{
+		jwt.StandardClaims{
+			Audience: "bad",
+		},
+	})
+
+	// Bad issuer value.
+	testTokenInvalidIssuer = makeToken(&Claims{
+		jwt.StandardClaims{
+			Issuer: "bad",
+		},
+	})
+
+	// Expired.
+	testTokenExpired = makeToken(&Claims{
+		jwt.StandardClaims{
+			ExpiresAt: 1000000000,
+		},
+	})
+
+	os.Exit(m.Run())
+}
