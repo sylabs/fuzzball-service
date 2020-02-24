27f51bc1c5a53abdb66815482668b6facb161671
Adam Hughes <stickmanica@gmail.com>
2020-02-24 11:22:40 -0500

    Improve viewer test

--- internal/pkg/resolver/viewer_test.go
+++ internal/pkg/resolver/viewer_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/sylabs/compute-service/internal/pkg/token"
 )
 
-func TestViewer(t *testing.T) {
+func TestViewerWorkflows(t *testing.T) {
 	// User token to pass in context.
 	tok := token.Token{
 		Token: jwt.NewWithClaims(jwt.SigningMethodNone, &token.Claims{
@@ -83,6 +83,202 @@ func TestViewer(t *testing.T) {
 			    id
 			    login
 			    workflows(after: $after, before: $before, first: $first, last: $last) {
+			      edges {
+			        cursor
+			        node {
+			          id
+			          name
+			        }
+			      }
+			      pageInfo {
+			        startCursor
+			        endCursor
+			        hasNextPage
+			        hasPreviousPage
+			      }
+			      totalCount
+			    }
+			  }
+			}`
+
+			res := s.Exec(ctx, q, "", tt.args)
+
+			if err := verifyGoldenJSON(t.Name(), res); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestViewerJobs(t *testing.T) {
+	// User token to pass in context.
+	tok := token.Token{
+		Token: jwt.NewWithClaims(jwt.SigningMethodNone, &token.Claims{
+			StandardClaims: jwt.StandardClaims{
+				Subject: "jimbob",
+			},
+			UserID: "507f1f77bcf86cd799439011",
+		}),
+	}
+	ctx := token.NewContext(context.Background(), &tok)
+
+	sc := "startCursor"
+	ec := "endCursor"
+	jp := core.JobsPage{
+		Jobs: []core.Job{
+			{
+				ID:   "id1",
+				Name: "name1",
+			},
+			{
+				ID:   "id2",
+				Name: "name2",
+			},
+		},
+		PageInfo: core.PageInfo{
+			StartCursor:     &sc,
+			EndCursor:       &ec,
+			HasNextPage:     true,
+			HasPreviousPage: false,
+		},
+		TotalCount: 2,
+	}
+
+	cursor := "cursorValue"
+	count := 2
+
+	tests := []struct {
+		name   string
+		args   map[string]interface{}
+		wantPA core.PageArgs
+	}{
+		{"NoArgs", nil, core.PageArgs{}},
+		{"After", map[string]interface{}{"after": cursor}, core.PageArgs{After: &cursor}},
+		{"Before", map[string]interface{}{"before": cursor}, core.PageArgs{Before: &cursor}},
+		{"First", map[string]interface{}{"first": count}, core.PageArgs{First: &count}},
+		{"Last", map[string]interface{}{"last": count}, core.PageArgs{Last: &count}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mc, err := getMockCore(mockCore{
+				p: mockPersister{
+					wantPA: tt.wantPA,
+					jp:     jp,
+				},
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			s, err := schema.Get(&Resolver{s: mc})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			q := `
+			query OpName($after: String, $before: String, $first: Int, $last: Int) {
+			  viewer {
+			    id
+			    login
+			    jobs(after: $after, before: $before, first: $first, last: $last) {
+			      edges {
+			        cursor
+			        node {
+			          id
+			          name
+			        }
+			      }
+			      pageInfo {
+			        startCursor
+			        endCursor
+			        hasNextPage
+			        hasPreviousPage
+			      }
+			      totalCount
+			    }
+			  }
+			}`
+
+			res := s.Exec(ctx, q, "", tt.args)
+
+			if err := verifyGoldenJSON(t.Name(), res); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestViewerVolumes(t *testing.T) {
+	// User token to pass in context.
+	tok := token.Token{
+		Token: jwt.NewWithClaims(jwt.SigningMethodNone, &token.Claims{
+			StandardClaims: jwt.StandardClaims{
+				Subject: "jimbob",
+			},
+			UserID: "507f1f77bcf86cd799439011",
+		}),
+	}
+	ctx := token.NewContext(context.Background(), &tok)
+
+	sc := "startCursor"
+	ec := "endCursor"
+	vp := core.VolumesPage{
+		Volumes: []core.Volume{
+			{
+				ID:   "id1",
+				Name: "name1",
+			},
+			{
+				ID:   "id2",
+				Name: "name2",
+			},
+		},
+		PageInfo: core.PageInfo{
+			StartCursor:     &sc,
+			EndCursor:       &ec,
+			HasNextPage:     true,
+			HasPreviousPage: false,
+		},
+		TotalCount: 2,
+	}
+
+	cursor := "cursorValue"
+	count := 2
+
+	tests := []struct {
+		name   string
+		args   map[string]interface{}
+		wantPA core.PageArgs
+	}{
+		{"NoArgs", nil, core.PageArgs{}},
+		{"After", map[string]interface{}{"after": cursor}, core.PageArgs{After: &cursor}},
+		{"Before", map[string]interface{}{"before": cursor}, core.PageArgs{Before: &cursor}},
+		{"First", map[string]interface{}{"first": count}, core.PageArgs{First: &count}},
+		{"Last", map[string]interface{}{"last": count}, core.PageArgs{Last: &count}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mc, err := getMockCore(mockCore{
+				p: mockPersister{
+					wantPA: tt.wantPA,
+					vp:     vp,
+				},
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			s, err := schema.Get(&Resolver{s: mc})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			q := `
+			query OpName($after: String, $before: String, $first: Int, $last: Int) {
+			  viewer {
+			    id
+			    login
+			    volumes(after: $after, before: $before, first: $first, last: $last) {
 			      edges {
 			        cursor
 			        node {
